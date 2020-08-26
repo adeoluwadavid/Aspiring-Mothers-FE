de06d330124b946ea5d0d9fae78b7ab972ce250f
adeoluwadavid <adeoluwadavid@gmail.com>
2020-08-26 10:17:22 +0100

    My Final Commit

--- src/components/profile.js
+++ src/components/profile.js
@@ -5,11 +5,14 @@ import Header from './dashboardheader'
 class Profile extends Component {
     state = {
         myToken: JSON.parse(localStorage.getItem('token')),
-        selectedFile : null,
+        selectedFile: null,
         image: [],
-        message:''
+        message: '',
+      //  currentPassword:'',
+        newPassword:'',
+        passwordMessage:''
     }
-    fileHandler =(e)=>{
+    fileHandler = (e) => {
         console.log(e.target.files[0])
         this.setState({
             selectedFile: e.target.files[0]
@@ -20,8 +23,8 @@ class Profile extends Component {
         var data = new FormData();
         data.append('file', this.state.selectedFile);
 
-        fetch("http://localhost:8088/authe/upload/" + parseInt(this.state.myToken.id), {  
-             method: 'PUT',
+        fetch("http://localhost:8088/authe/upload/" + parseInt(this.state.myToken.id), {
+            method: 'PUT',
             body: data
         })
             .then((res) => res.json())
@@ -31,41 +34,100 @@ class Profile extends Component {
         })
     }
 
-    componentDidMount(){
-        fetch('http://localhost:8088/authe/download/'+ parseInt(this.state.myToken.id))
-        .then(res => res.json())
-        .then(data =>{ 
-            if(data){
-            this.setState({
-                image: data
-            })
-        }
+    //Password Reset
+    // change1=(e)=>{
+    //     this.setState({
+    //         currentPassword: e.target.value
+    //     })
+    //     console.log(e.target.value)
+    // }
+    change2=(e)=>{
+        this.setState({
+            newPassword: e.target.value
         })
+        console.log(e.target.value)
+    }
+
+    updatePassword=(e)=>{
+        e.preventDefault();
+        const user ={
+           // currentPassword: this.state.currentPassword,
+            newPassword: this.state.newPassword
+        }
+
+        fetch('http://localhost:8088/authe/updatePassword/' + parseInt(this.state.myToken.id),{
+            method:'PUT',
+            headers:{
+                'Accept': 'application/json, text/plain, */*',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(user) 
+        })
+        .then(res => res.text())
+        .then(data => console.log(data))
+
+        this.setState({
+        //    currentPassword: '',
+            newPassword: '',
+            passwordMessage: 'Your Password Has Been Reset Successfully'
+        })
+    }
+    componentDidMount() {
+        fetch('http://localhost:8088/authe/download/' + parseInt(this.state.myToken.id))
+            .then(res => res.json())
+            .then(data => {
+                if (data) {
+                    this.setState({
+                        image: data
+                    })
+                }
+            })
     }
 
     render() {
         const myImage = this.state.image;
-        console.log(myImage)
+       // console.log(myImage)
         return (
             <div className="container">
                 <Header />
+               
                 <div className="row">
-                    <br />
-                    <div  className="center">
-                    <img className="logo circle" src={myImage.imageUrl} alt="Logo" /> 
-                    </div>
-                    <div className="col s12 center" >
-                        <div className="row">
-                            <div className="input-field">
-                                <input type="file" name="file" onChange={this.fileHandler} />
+                    <div className="col l6">
+                        <br />
+                        <div className="center">
+                            <img className="logo circle" src={myImage.imageUrl} alt="Logo" />
+                        </div>
+                        <div className="col s12 center" >
+                            <div className="row">
+                                <div className="input-field">
+                                    <input type="file" name="file" onChange={this.fileHandler} />
+                                </div>
+                                <div>
+                                    <button onClick={this.handleSubmit} className="btn">Upload</button>
+                                </div>
+                                <div className="center orange-text">{this.state.message}</div>
                             </div>
-                            <div>
-                                <button onClick={this.handleSubmit} className="btn">Upload</button>
-                            </div>
-                            <div className="center orange-text">{this.state.message}</div>
                         </div>
                     </div>
+                    <div className="col l6">
+                        <form className="col s12 center" onSubmit={this.updatePassword}>
+                            {/* <div className="input-field">
+                                <label>Current Password:</label>
+                                <input type="text" value={this.state.currentPassword} onChange={this.change1} required/>
+                            </div> */}
+                            <h5>Feeling like updating your Password?</h5>
+                            <div className="input-field">
+                                <label>New Password:</label>
+                                <input type="text" value={this.state.newPassword} onChange={this.change2}  required/>
+                            </div>
+                            <div>{this.state.passwordMessage}</div>
+                            <div>
+                                <button className="btn">Change Password</button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
+                
                 <Footer />
             </div>
         )
--- src/components/recoverpassword1.js
+++ src/components/recoverpassword1.js
@@ -3,7 +3,39 @@ import {Link} from 'react-router-dom'
 import logo from '../img/logo.png'
 import Footer from './footer'
 class RecoverPasswordOne extends Component {
+    state={
+            email: '',
+            message: 'Please Enter a Correct Mail'
+    }
+    handleChange=(e)=>{
+        this.setState({
+            email : e.target.value
+        })
+        console.log(e.target.value);
+    }
+    handleSubmit=(e)=>{
+        e.preventDefault();
+        const user ={
+            email: this.state.email
+        }
+        fetch('http://localhost:8088/authe/setPassword', {
+            method:'POST',
+            headers:{
+                'Acccept': 'application/json, text/plain, */*',
+                'Content-Type':'application/json'
+            },
+            body: JSON.stringify(user)
+        })
+        .then(res => res.text())
+        .then(data =>console.log(data))
+
+        this.setState({
+            email: '',
+            message:'Your Password Has Been Reset and Mail Sent!'
+        })
+    }
     render() {
+        
         return (
             <div className="container">
                 <a href="/"> 
@@ -11,15 +43,17 @@ class RecoverPasswordOne extends Component {
                   <img className="" src={logo} id="logo" alt="Logo" /> 
                 </div>
                 </a>
-
+                <form  className="col s12" onSubmit={this.handleSubmit}>
                 <div className="row">
                     <div className="input-field center">
-                        <input id="first_name2" type="email" className="validate" />
-                        <label className="active" for="recoverPassword">Your Email</label>
+                        <input id="first_name2" type="email" className="validate" value={this.state.email} onChange={this.handleChange} />
+                        <label className="active" htmlFor="recoverPassword">Your Email</label>
                     </div>
-                </div>
-                <div>
+                    <div className="center red-text">{this.state.message}</div>
                     <button className="btn button waves-effect waves-light">Recover Password</button><br />
+                </div>
+                </form>
+                <div>
                     <div className="row">
                         <Link to="/login">
                             <button className="btn button waves-effect waves-light col s6">Login</button>
@@ -29,6 +63,7 @@ class RecoverPasswordOne extends Component {
                         </Link>
                     </div>
                 </div>
+                
                 <Footer />
             </div>
         )
